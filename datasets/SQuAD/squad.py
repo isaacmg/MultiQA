6cd459de0927a3cbef4d6b3128dc73620af247ca
Isaac Godfried <igodfried@isaac26.com>
2019-08-09 01:15:19 -0400

    fix squad

--- datasets/SQuAD/squad.py
+++ datasets/SQuAD/squad.py
@@ -8,7 +8,6 @@ import tqdm
 
 class SQuAD(MultiQA_DataSet):
     """
-
     """
 
     def __init__(self):
@@ -40,8 +39,9 @@ class SQuAD(MultiQA_DataSet):
 
     @overrides
     def build_contexts(self, preprocessor, split, sample_size, dataset_version, dataset_flavor, dataset_specific_props, input_file):
-        single_file_path = cached_path("https://rajpurkar.github.io/SQuAD-explorer/dataset/" + 		         
-                                        split + "-v" + dataset_version.replace('-','.') +".json")	
+        single_file_path = cached_path("https://rajpurkar.github.io/SQuAD-explorer/dataset/" + \
+                                       split + "-v" + dataset_version.replace('-','.') +".json")
+
         with open(single_file_path, 'r') as myfile:
             original_dataset = json.load(myfile)
 
